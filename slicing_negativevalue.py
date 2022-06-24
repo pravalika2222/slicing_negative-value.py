af8c5647ffa377c157c9d40d00b77e7ca1385db9
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-06-24 13:37:58 +0530

    Add files via upload

---
+++ slicing_negativevalue.py
@@ -0,0 +1,34 @@
+# a='beautiful'
+# print(a[-9:])   # beautiful   # here we using left side values as a negative values but, it convert into positive value & right side should take total string if we don't gave a value
+# print(a[-8:])   # eautiful
+# print(a[-7:])   # autiful
+# print(a[-6:])   # utiful
+# print(a[-5:])   # tiful
+# print(a[-4:])   # iful
+# print(a[-3:])   # ful
+# print(a[-2:])   # ul
+# print(a[-1:])   # l
+# print(a[-11:])    #  in negative values,if thw index value is not there it will take the next coming index value 
+
+# print(a[-6:-4])  # ut   
+# # print(a[-7:-5])  # au 
+# print(a[-9:-3])  # beauti
+
+# print(a[-2:-3])   #  blank line (left side value is greaterthan the right side value so, it willshowa blank line )
+
+# "slicing double colon syntax"
+# pirnt(a[starting index value:ending index value:spliting times])
+# print(A[::])
+
+# a='biriyani'
+# print(a[::])  # biriyani (double colon with empty indicates,it will take default value as starting,ending,index value& splinting times)
+
+# print(a[::-1])  # inayirib  # (doubble colon indicates the reverse order of string)
+# print(a[::-2])  # iaii # skipping 2 times 
+# print(a[::-3])  # iyi
+# print(a[::-4])  # ii
+# print(a[::-5])  # ir
+# print(a[::-6])  # ii
+# print(a[::-7])  # ib
+# print(a[::-8])  # i   
+ 
